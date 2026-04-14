dac5bb83ce7282dffdf3d140b76d676804ea00fc
KE-Holtz <keholtz08@gmail.com>
2026-04-14 11:51:36 -0400

    editors.nix

--- home/code/editors.nix
+++ home/code/editors.nix
@@ -1,5 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  programs.vscode.enable = true;
+  programs.vscode = {
+      enable = true;
+      userSettings = {
+      "java.jdt.ls.java.home" = "${pkgs.jdk21}/lib/openjdk";
+    };
+  };
 }
