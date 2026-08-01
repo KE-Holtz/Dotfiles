c70df0bd7f3cefb624332cac080a7195f45365d9
KE-Holtz <keholtz08@gmail.com>
2026-08-01 17:10:49 -0400

    Added various apps

--- home/home.nix
+++ home/home.nix
@@ -4,6 +4,9 @@
   home.username = "kyle";
   home.homeDirectory = "/home/kyle";
   home.stateVersion = "25.05";
+  home.sessionVariables = {
+    LEDGER_FILE = "$HOME/Documents/Finance/journal.hledger";
+  };
 
   imports = [ rice/default.nix ./coding.nix ./shell.nix inputs.zen-browser.homeModules.twilight];
 
@@ -22,12 +25,29 @@
     pkgs.cowsay
     pkgs.prismlauncher
     pkgs.gimp
+    pkgs.blender
     pkgs.kdePackages.kdenlive
     pkgs.kdePackages.kdeconnect-kde
     pkgs.vlc
+    pkgs.audacity
     pkgs.yazi
     pkgs.thunar
     pkgs.btop
+    pkgs.hledger
+    pkgs.hledger-web
   ];
 
+  programs.obs-studio = {
+    enable = true;
+
+    # optional Nvidia hardware acceleration
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-vaapi #optional AMD hardware acceleration
+      obs-gstreamer
+      obs-vkcapture
+    ];
+  };
 }
